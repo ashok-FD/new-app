d848c4d3300b2eddccd144c48bfea9aef94a8e7d
Ashok <ashokask186@gmail.com>
2020-11-29 00:47:04 +0530

    first commit

---
+++ src/Components/MyComponent.js
@@ -0,0 +1,10 @@
+// MyComponent.js
+import React, { Component } from "react";
+
+class MyComponent extends Component {
+  render() {
+    return <div>This is my component.</div>;
+  }
+}
+
+export default MyComponent;
